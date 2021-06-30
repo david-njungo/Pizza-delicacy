c18e3d0616d725f1a8b4ecac11e9a87f081d9050
David Njungo <davidnjungo3@gmail.com>
2021-06-30 15:43:30 +0300

    add the function orderPizza

--- script.js/script.js
+++ script.js/script.js
@@ -13,3 +13,35 @@ function getData() {
       alert("You're welcome to pick up your package.Thankyou");
     }
   }
+  function orderPizza(sizeAmount, crustAmount, toppings) {
+    this.sizeAmount = sizeAmount;
+    this.crustAmount = crustAmount;
+    this.toppings = toppings;
+  
+    var small = document.getElementById("small")
+    if (small.checked == true) {
+      var sizeAmount = 400;
+    }
+    var large = document.getElementById("large")
+    if (large.checked == true) {
+      var sizeAmount = 800;
+    }
+    var medium = document.getElementById("medium")
+    if (medium.checked == true) {
+      var sizeAmount = 600;
+    }
+    var crispy = document.getElementById("crispy")
+    if (crispy.checked == true) {
+      var crustAmount = 100;
+    }
+    var stuffed = document.getElementById("stuffed")
+    if (stuffed.checked == true) {
+      var crustAmount = 150;
+    }
+    var gluttenfree = document.getElementById("gluttenfree")
+    if (gluttenfree.checked == true) {
+      var crustAmount = 120;
+    }
+    var total = sizeAmount + crustAmount;
+    document.getElementById("total").innerHTML = ("Your total charge is; Ksh " + total + ".")
+  }
